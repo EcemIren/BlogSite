bd45c994bdf445be0041937fe3a48a1b1e8274f8
Ecem İren <gizem.iren@hotmail.com>
2017-10-05 18:36:31 +0300

    Partial Views of Tags, Categories, Search and AboutSite are created

---
+++ BlogSite/Controllers/TagController.cs
@@ -0,0 +1,24 @@
+﻿using BlogSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogSite.Controllers
+{
+    public class TagController : Controller
+    {
+        BlogContext context = new BlogContext();
+        // GET: Tag
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult TagWidget()
+        {
+            return PartialView(context.Tags.ToList());
+        }
+    }
+}
